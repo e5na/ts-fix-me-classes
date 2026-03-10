de2db1e2dc1dc9f6d8a2f55efea9956c269b5392
Marko Esna <marko.esna@gmail.com>
2026-03-10 10:27:43 +0200

    fixed all files

--- src/person.ts
+++ src/person.ts
@@ -1,18 +1,18 @@
 class Person {
-    name: string
-    age: number
+    name: string;
+    age: number;
 
-    constructor(name: string, age: string) {
-        this.Name = name
-        this.age = age
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
     }
 
     getName(): string {
-        return this.Name
+        return this.name;
     }
 
     getAge(): number {
-        return age
+        return this.age;
     }
 }
 
